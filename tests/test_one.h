98639a098695d1b398333d4751e9d0716da71420
beltyugo <36560221+beltyugo@users.noreply.github.com>
2018-06-19 21:34:59 +0300

    Update test_one.h
    dd

--- tests/test_one.h
+++ tests/test_one.h
@@ -7,6 +7,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+
 extern "C" {
 #include "common.h"
 #include "text.h"
@@ -15,83 +16,98 @@ extern "C" {
 }
 
 
-TEST(test_one_h, one)
-{
+//test run
+TEST (Showlengths, suite1) {
     text txt = create_text();
     showlengths(txt);
     SUCCEED();
 }
 
-TEST(protest, show_lengths)
+//3 words on 3 lines
+TEST (Showlengths, suite2) {
 
-{
-     printf("good");
-     int fptr;
+    text txt = create_text();
 
-     int oldstdout;
+    char *filedir=(char*)malloc(256);
+    sprintf(filedir, "%s/text.txt", TESTIDIR);
+    load(txt, filedir);
 
-     fptr = open("testwork.txt",O_CREAT|O_RDWR,S_IREAD|S_IWRITE);
+    int old_dup;
+    int out_func = open("testwork.txt",O_CREAT|O_RDWR,S_IREAD|S_IWRITE);
+    old_dup = dup(STDOUT);
+    dup2(out_func, STDOUT);
 
-     oldstdout = dup(STDOUT);
+    showlengths(txt);
 
-     dup2(fptr,STDOUT);
+    fflush(stdout);
+    close(out_func);
 
-     char *filedir=(char*)malloc(1024);
+    dup2(old_dup, STDOUT);
 
-     sprintf(filedir, "%s/text.txt", TESTIDIR);
+    FILE *testwork;
+    testwork = fopen("testwork.txt","rw");
+    if(testwork == NULL){
+           FAIL();
+           return;
+    }
 
-     text txt = create_text();
+    char *buf1 = (char*)malloc(sizeof(char)*512);
+    int readcount1 = fread(buf1,1,512,testwork);
 
-     load(txt, filedir);
-     show(txt);
-     showlengths(txt);
+    char *checkdir=(char*)malloc(256);
+    FILE *check;
+    sprintf(checkdir, "%s/proverka.txt", TESTIDIR);
+    check = fopen(checkdir,"rw");
+    if(check == NULL){
+           FAIL();
+           return;
+    }
 
-close(fptr);
+    char *buf2 = (char*)malloc(sizeof(char)*512);
+    int readcount2 = fread(buf2,1,512,check);
 
-dup2(oldstdout,STDOUT);
+    fclose(testwork);
+    fclose(check);
+    remove("testwork.txt");
 
-     FILE *t2;
-
-     t2 = fopen("testwork.txt","rw");
-
-     if(t2 == NULL){
-
-            FAIL();
-
-            return;
-
-     }
-
-     char *buf = (char*)malloc(sizeof(char)*512);
-
-     int readcount = fread(buf,1,512,t2);
-
-     FILE *d2;
-
-     sprintf(filedir, "%s/proverka.txt", TESTIDIR);
-
-     d2 =fopen(filedir,"rw");
-
-     if(d2 == NULL){
-
-            FAIL();
-
-            return;
-
-     }
-
-     char *buf2 = (char*)malloc(sizeof(char)*512);
-
-     int readcount2 = fread(buf2,1,512,d2);
-
-     fclose(t2);
-
-     fclose(d2);
-
-     ASSERT_EQ(readcount,readcount2);
-
+    EXPECT_EQ(readcount2, readcount1);
 }
 
+//Empty test file
+TEST (Showlengths, suite3) {
 
+    text txt = create_text();
+
+    char *filedir=(char*)malloc(256);
+    sprintf(filedir, "%s/text2.txt", TESTIDIR);
+    load(txt, filedir);
+
+    int old_dup;
+    int out_func = open("testwork2.txt",O_CREAT|O_RDWR,S_IREAD|S_IWRITE);
+    old_dup = dup(STDOUT);
+    dup2(out_func, STDOUT);
+
+    showlengths(txt);
+
+    fflush(stdout);
+    close(out_func);
+
+    dup2(old_dup, STDOUT);
+
+    FILE *testwork;
+    testwork = fopen("testwork2.txt","rw");
+    if(testwork == NULL){
+           FAIL();
+           return;
+    }
+
+    char *buf = (char*)malloc(sizeof(char)*512);
+    int readcount = fread(buf,1,512,testwork);
+
+    fclose(testwork);
+    remove("testwork2.txt");
+
+    EXPECT_EQ(0, readcount);
+}
 
 #endif // TEST_ONE_H
